f30d94fda028f09df7fdcf1abd5b0541407f1a7c
Johnny Deymisson <jdeymisson@gmail.com>
2022-05-16 21:20:15 -0300

    Funcionalidades SPA finalizadas

--- js/router.js
+++ js/router.js
@@ -1,19 +1,28 @@
 export class Router {
   routes = {};
 
+  add(routeName, page) {
+    this.routes[routeName] = page;
+  }
+
   route(event) {
     event = event || window.event;
     event.preventDefault();
+    window.history.pushState({}, '', event.target.href);
 
-
+    this.handle();
   };
 
   handle() {
     const { pathname } = window.location;
 
-    const route = routes[pathname] || routes[404];
+    const route = this.routes[pathname] || this.routes[404];
 
-    console.log(route);
+    fetch(route)
+    .then(response => response.text())
+    .then(html => {
+      document.querySelector('#app').innerHTML = html;
+    });
   };
 
 };
