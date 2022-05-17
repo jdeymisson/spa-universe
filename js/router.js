ca5b8edd885955ec0dd0550b5718b9f43c042324
Ben-Hur <benhur@a52.com.br>
2022-05-17 11:26:57 -0300

    finalizando as funções no JS e subindo o css

--- js/router.js
+++ js/router.js
@@ -10,13 +10,53 @@ export class Router {
     event.preventDefault();
     window.history.pushState({}, '', event.target.href);
 
-    this.handle();
+    this.handle(event.target);
   };
 
-  handle() {
+  handle(event) {
     const { pathname } = window.location;
 
     const route = this.routes[pathname] || this.routes[404];
+    const body = document.querySelector('body');
+    const listMenu = document.querySelectorAll('ul li a'); 
+    
+    let bodyClass = pathname.replace('/', '');
+    
+
+    if(bodyClass == '') {
+      body.className = '';
+      body.classList.add('home');
+
+      listMenu.forEach((item) => {
+        item.classList.remove('active');
+      });
+
+      listMenu[0].classList.add('active');
+      
+    } else if (bodyClass == 'universe') {
+      body.className = '';
+      body.classList.add(bodyClass);
+
+      listMenu.forEach((item) => {
+        item.classList.remove('active');
+      });
+
+      listMenu[1].classList.add('active');
+
+    } else if (bodyClass == 'exploration') {
+      body.className = '';
+      body.classList.add(bodyClass);
+
+      listMenu.forEach((item) => {
+        item.classList.remove('active');
+      });
+
+      listMenu[2].classList.add('active');
+      
+    } else if (bodyClass !== '' || 'universe' || 'explortion') {
+      body.className = '';
+      body.classList.add('notfound');
+    }
 
     fetch(route)
     .then(response => response.text())
